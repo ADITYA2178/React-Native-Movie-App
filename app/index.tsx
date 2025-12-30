66c572eb513b584c5d545688bb0979466e95500a
Adityaj2902 <adityajofficial909@gmail.com>
2025-12-30 23:38:20 +0530

    Theme File Has been Updated with color

--- app/index.tsx
+++ app/index.tsx
@@ -2,8 +2,8 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text className="text-2xl text-red-500">Hiii.</Text>
+    <View className="flex-1 items-center justify-center bg-dark-100">
+      <Text className="text-2xl font-extrabold text-red-500">Hii Jaiswal How are You Aditya </Text>
     </View>
   );
 }
